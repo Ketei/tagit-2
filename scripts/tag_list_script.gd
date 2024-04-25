421288de2fdf74d92d6fb9ba0d9caba64c221980
Ketei <ketei.ilust@gmail.com>
2024-04-25 00:40:46 -0600

    Fixed quick delete + press
    When deleting a tag/tag group, if you quickly ´pressed -> pressed del -> pressed´ a tag it would activate the tag.

--- scripts/tag_list_script.gd
+++ scripts/tag_list_script.gd
@@ -7,22 +7,29 @@ signal list_emptied
 
 
 @export var deselect_on_focus_lost: bool = true
-
+var delete_timer: Timer
 
 func _ready():
 	focus_exited.connect(on_focus_lost)
+	delete_timer = Timer.new()
+	delete_timer.wait_time = 0.1
+	delete_timer.autostart = false
+	delete_timer.one_shot = true
+	add_child(delete_timer)
 
 
 func _gui_input(_event):
 	if has_focus() and Input.is_action_just_pressed("ui_text_delete") and is_anything_selected():
-		var selected_items: PackedInt32Array = get_selected_items()
-		selected_items.reverse()
+		remove_indexes(get_selected_items())
+		#var selected_items: PackedInt32Array = get_selected_items()
+		#selected_items.reverse()
 		
-		for index in selected_items:
-			item_deleted.emit(get_item_text(index))
-			remove_item(index)
-		if item_count == 0:
-			list_emptied.emit()
+		#for index in selected_items:
+			#item_deleted.emit(get_item_text(index))
+			#remove_item(index)
+		#if item_count == 0:
+			#list_emptied.emit()
+		delete_timer.start()
 		deselect_all()
 
 
@@ -61,9 +68,9 @@ func on_focus_lost() -> void:
 
 
 func remove_indexes(indexes_to_remove: Array[int]) -> void:
-	var _indexes: Array[int] = indexes_to_remove.duplicate()
+	var _indexes: Array[int] = []
+	_indexes.assign(indexes_to_remove)
 	_indexes.sort_custom(func(a, b): return a > b)
 	for index in _indexes:
 		remove_item(index)
-	deselect_all()
 
