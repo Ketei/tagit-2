c27031914a743f8d8180e9a225b2b3b22f023dc8
Ketei <ketei.ilust@gmail.com>
2024-05-02 16:17:17 -0600

    Better age tagging

--- scripts/age_option_button.gd
+++ scripts/age_option_button.gd
@@ -1,26 +1,46 @@
 class_name AgeOptionButton
 extends OptionButton
 
+var default_age: String = "adult"
+
+enum AgeRange{
+	YOUNG,
+	ADULT,
+	MATURE,
+	OLD
+}
+
 const AGES: Dictionary = {
-	"adult": {"name": "Adult", "tag": ""},
-	"teen": {"name": "Adolescent", "tag": "adolescent"},
-	"child": {"name": "Child", "tag": "child"},
-	"todd": {"name": "Toddler", "tag": "toddler"},
-	"bab": {"name": "Baby", "tag": "baby"}}
+	"old": {"name": "Elderly", "tag": "old", "cat": AgeRange.OLD},
+	"mature": {"name": "Mature", "tag": "", "cat": AgeRange.MATURE},
+	"adult": {"name": "Adult", "tag": "", "cat": AgeRange.ADULT},
+	"young_adult": {"name": "Young Adult", "tag": "young adult", "cat": AgeRange.ADULT},
+	"teen": {"name": "Adolescent", "tag": "adolescent", "cat": AgeRange.YOUNG},
+	"child": {"name": "Child", "tag": "child", "cat": AgeRange.YOUNG},
+	"todd": {"name": "Toddler", "tag": "toddler", "cat": AgeRange.YOUNG},
+	"bab": {"name": "Baby", "tag": "baby", "cat": AgeRange.YOUNG}}
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var index_counter: int = 0
+	var default_select: int = 0
 	for age in AGES:
 		add_item(AGES[age]["name"])
 		set_item_metadata(
 				index_counter,
-				AGES[age]["tag"])
+				{"tag": AGES[age]["tag"], "range": AGES[age]["cat"]})
+		if age == default_age:
+			default_select = index_counter
 		index_counter += 1
-	select(0)
+	select(default_select)
 
 
 func get_age_tag() -> String:
-	return get_item_metadata(selected)
+	return get_item_metadata(selected)["tag"]
+
+
+func get_age_category() -> AgeRange:
+	return get_item_metadata(selected)["range"]
+
 
