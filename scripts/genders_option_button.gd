67481f868ddcc0d075ad0519e9bc6d7b2410060d
Ketei <ketei.ilust@gmail.com>
2024-05-23 18:53:55 -0600

    Fixed some stuff

--- scripts/genders_option_button.gd
+++ scripts/genders_option_button.gd
@@ -2,13 +2,13 @@ class_name GenderOptionButton
 extends OptionButton
 
 const GENDERS: Dictionary = {
-	"male": {"name": "Male", "tag": "male"},
-	"female": {"name": "Female", "tag": "female"},
-	"amb": {"name": "Ambiguous", "tag": "ambiguous gender"},
-	"andro": {"name": "Andromorph", "tag": "andromorph"},
-	"gyno": {"name": "Gynomorph", "tag": "gynomorph"},
-	"herm": {"name": "Hermaphrodite", "tag": "herm"},
-	"maleherm": {"name": "Male Hermaphrodite", "tag": "maleherm"}
+	"male": {"name": "Male", "tag": "male", "is_female": false},
+	"female": {"name": "Female", "tag": "female", "is_female": true},
+	"amb": {"name": "Ambiguous", "tag": "ambiguous gender", "is_female": false},
+	"andro": {"name": "Andromorph", "tag": "andromorph", "is_female": false},
+	"gyno": {"name": "Gynomorph", "tag": "gynomorph", "is_female": true},
+	"herm": {"name": "Hermaphrodite", "tag": "herm", "is_female": true},
+	"maleherm": {"name": "Male Hermaphrodite", "tag": "maleherm", "is_female": false}
 }
 
 
@@ -26,6 +26,9 @@ func _ready():
 func get_gender_tag() -> String:
 	return GENDERS[get_item_metadata(selected)]["tag"]
 
+
+func is_female() -> bool:
+	return GENDERS[get_item_metadata(selected)]["is_female"]
 
 
 
