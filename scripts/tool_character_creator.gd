7e797720af8c582bbb4ff885f8c0c5f4b0c17ead
Ketei <ketei.ilust@gmail.com>
2024-05-23 18:53:38 -0600

    Extended functionality
    * If no name is provided it'll default to "nameless character"
    * If selected shortstack/teapot it'll add the appropriate depending on the gender.
    * If wiki is empty it'll add default info
    * The info changes depending on the screen you're at
    * If you're replacing a tag it'll warn you first
    * Once the tag is created it'll notify you.
    * Extra tags are now added

--- scripts/tool_character_creator.gd
+++ scripts/tool_character_creator.gd
@@ -1,6 +1,17 @@
 extends Control
 
 
+const TAB_INFO: Dictionary = {
+	1: "Basic Info Tab",
+	2: "Heaad Info Tab",
+	3: "Torso Info Tab",
+	4: "Hand/Feet Info Tab",
+	5: "Legs Info Tab",
+	6: "Tail info Tab",
+	7: "Genitals Info Tab",
+	8: "Extra Info Tab",
+}
+
 
 @onready var category_info: RichTextLabel = $MarginContainer/DataContainer/InfoContainer/PanelContainer/InfoSide/InfoSmoothScrollContainer/CategoryInfo
 @onready var tag_item_list: TagItemList = $MarginContainer/DataContainer/InfoContainer/ExtraTagsContainer/TagItemList
@@ -22,6 +33,7 @@ extends Control
 @onready var age_option_button: AgeOptionButton = $MarginContainer/DataContainer/TabsContainer/InteractSide/SmoothScrollContainer/TabContainer/BaseData/BasicsContainer/AgeContainer/AgeOptionButton
 @onready var body_type_opt_btn: BodyOptionButton = $MarginContainer/DataContainer/TabsContainer/InteractSide/SmoothScrollContainer/TabContainer/BaseData/BasicsContainer/BodyForm/BodTypeOptBtn
 
+@onready var figure_button: TagsOptionButton = $MarginContainer/DataContainer/TabsContainer/InteractSide/SmoothScrollContainer/TabContainer/BaseData/ShapeContainer/FigureContainer/TagsOptionButton
 
 
 var skip_nodes: Array[Control] = []
@@ -44,9 +56,13 @@ func _ready():
 	next_button.pressed.connect(on_next_pressed)
 	prev_button.pressed.connect(on_previous_pressed)
 	finish_button.pressed.connect(on_finish_pressed)
+	reset_button.pressed.connect(reset_fields)
 	max_tabs = tab_container.get_child_count()
+	
 	if tab_container.current_tab != 0:
 		tab_container.current_tab = 0
+	
+	category_info.text = TAB_INFO[current_tab]
 	
 	skip_nodes.assign([
 			wiki_text_edit,
@@ -65,8 +81,34 @@ func _ready():
 
 
 func generate_characer() -> void:
+	var tag_name: String = name_line_edit.text.strip_edges().to_lower()
+	
+	if Tagger.has_tag(tag_name):
+		var warn_window:TaggerConfirmationDialog = Tagger.create_confirmation_dialog()
+		
+		warn_window.set_data(
+			"Existing Tag...",
+			"Tag \"{0}\" already exists.\nOverwrite?".format([tag_name]),
+			"Overwrite",
+			"Cancel")
+		
+		warn_window.visible = true
+		
+		var replace_tag: bool = await warn_window.dialog_confirmed
+		
+		warn_window.visible = false
+		warn_window.queue_free()
+		
+		if not replace_tag:
+			return
+	
+	
 	var tag_res := Tag.new()
-	tag_res.tag = name_line_edit.text.strip_edges()
+	tag_res.tag = tag_name
+	
+	if tag_res.tag.is_empty():
+		tag_res.tag = "nameless character"
+	
 	tag_res.category = Tagger.Categories.CHARACTER
 	tag_res.tag_priority = 5
 	DumbUtils.append_without_repeats(tag_res.parents, species_line_edit.get_string_array())
@@ -75,13 +117,21 @@ func generate_characer() -> void:
 	if not age.is_empty():
 		tag_res.parents.append(age)
 	DumbUtils.append_without_repeats(tag_res.parents, body_type_opt_btn.get_body_type())
+
+	if figure_button.selected == 5: # 4 base + unknown
+		if gender_option_button.is_female():
+			if not tag_res.suggestions.has("shortstack"):
+				tag_res.suggestions.append("shortstack")
+		else:
+			if not tag_res.suggestions.has("teapot (body type)"):
+				tag_res.suggestions.append("teapot (body type)")
 	
 	for opt_btn in tag_option_buttons:
 		DumbUtils.append_without_repeats(tag_res.suggestions, opt_btn.get_tags())
 	
 	for char_spin in character_spinboxes:
 		var tag_to_add: String = char_spin.get_tag()
-		if not tag_res.suggestions.has(tag_to_add):
+		if not tag_res.suggestions.has(tag_to_add) and not tag_to_add.is_empty():
 			tag_res.suggestions.append(tag_to_add)
 	
 	for char_flow in character_flows:
@@ -100,7 +150,13 @@ func generate_characer() -> void:
 				special.get_tags()
 		)
 	
+	DumbUtils.append_without_repeats(tag_res.suggestions, tag_item_list.get_tag_array())
+	
 	tag_res.wiki_entry = wiki_text_edit.text.strip_edges()
+	
+	if tag_res.wiki_entry.is_empty():
+		tag_res.wiki_entry = "- Nobody here but us chickens! -"
+	
 	tag_res.tooltip = tooltip_line_edit.text.strip_edges()
 	
 	print("Tag: " + tag_res.tag)
@@ -108,6 +164,12 @@ func generate_characer() -> void:
 	print("Suggestions: " + str(tag_res.suggestions))
 	print("Wiki: " + tag_res.wiki_entry)
 	print("Tooltip: " + tag_res.tooltip)
+	
+	Tagger.queue_notification(
+			"Character created",
+			"Character \"{0}\" has been created".format([tag_name]))
+	
+	
 
 
 func reset_fields() -> void:
@@ -121,6 +183,15 @@ func reset_fields() -> void:
 		char_chkbx.button_pressed = char_chkbx.default
 	for spec_item in conditionals:
 		spec_item.reset_items()
+	wiki_text_edit.clear()
+	tooltip_line_edit.clear()
+	name_line_edit.clear()
+	species_line_edit.clear()
+	gender_option_button.select(0)
+	age_option_button.select(0)
+	body_type_opt_btn.select(0)
+	tag_item_list.clear()
+	
 
 
 func on_next_pressed() -> void:
@@ -137,6 +208,7 @@ func on_tab_changed() -> void:
 	next_button.visible = current_tab != max_tabs
 	finish_button.visible = current_tab == max_tabs
 	prev_button.disabled = current_tab == 1
+	category_info.text = TAB_INFO[current_tab]
 	queue_scroll_to_top()
 
 
