bfd3639cc940cb763f26c56f0a32b8726c665626
Ketei <ketei.ilust@gmail.com>
2024-07-20 15:00:00 -0600

    Bumped version.
    Since a feature was added, a Y version was +1ed

--- autoload/settings.gd
+++ autoload/settings.gd
@@ -146,7 +146,7 @@ const WIKI: String = "https://e621.net/wiki_pages.json?limit=1&title=" # title
 const TAGS: String = "https://e621.net/tags.json?"
 const ALIASES: String = "https://e621.net/tag_aliases.json?search[name_matches]="
 const PARENTS: String = "https://e621.net/tag_implications.json?search[antecedent_name]="
-const VERSION: String = "2.4.2"
+const VERSION: String = "2.5.0"
 const HEADER_FORMAT: String = "TaglistMaker/{0} (by Ketei)"
 const AUTOFILL_TIME: float = 0.3
 const GENDERS: Dictionary = {
